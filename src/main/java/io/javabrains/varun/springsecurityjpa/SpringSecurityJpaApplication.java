5856aa7c2951d39f51605ef0353316f8eff95e9a
Varun Jaiswal <varjaisw@cisco.com>
2023-05-10 16:55:47 +0530

    added changes without JPA

--- src/main/java/io/javabrains/varun/springsecurityjpa/SpringSecurityJpaApplication.java
+++ src/main/java/io/javabrains/varun/springsecurityjpa/SpringSecurityJpaApplication.java
@@ -2,8 +2,9 @@ package io.javabrains.varun.springsecurityjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SpringSecurityJpaApplication {
 
 	public static void main(String[] args) {
